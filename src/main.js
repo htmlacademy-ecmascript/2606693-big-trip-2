9a8401d61cfeed920484cd961c064c2e50c47446
chlvk <ochlvk@gmail.com>
2025-07-12 23:06:30 +0300

    Добавляет решение для Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import PointsModel from './model/points-model.js';
 import { render, RenderPosition } from './render.js';
 import ListFilterView from './view/list-filter-view.js';
 import TripInfoView from './view/trip-info-view.js';
@@ -7,9 +8,11 @@ const tripMainElement = document.querySelector('.trip-main');
 const filtersContainerElement = tripMainElement.querySelector('.trip-controls__filters');
 const pointsContainerElement = document.querySelector('.trip-events');
 
-const tablePresenter = new TablePresenter({container: pointsContainerElement});
+const pointsModel = new PointsModel();
+const tablePresenter = new TablePresenter({container: pointsContainerElement, pointsModel});
 
 render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
 render(new ListFilterView(), filtersContainerElement);
 
+pointsModel.init();
 tablePresenter.init();
--- src/presenter/table-presenter.js
+++ src/presenter/table-presenter.js
@@ -4,22 +4,36 @@ import PointEditView from '../view/point-edit-view.js';
 import PointsListView from '../view/points-list-view.js';
 import PointView from '../view/point-view.js';
 
-const POINTS_COUNT = 3;
-
 class TablePresenter {
   pointsListComponent = new PointsListView();
 
-  constructor({container}) {
+  constructor({container, pointsModel}) {
     this.container = container;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.points = [...this.pointsModel.getPoints()];
+    this.destinations = [...this.pointsModel.getDestinations()];
+    this.offers = [...this.pointsModel.getOffers()];
+
     render(new ListSortView(), this.container);
     render(this.pointsListComponent, this.container);
-    render(new PointEditView(), this.pointsListComponent.getElement());
 
-    for (let i = 0; i < POINTS_COUNT; i++) {
-      render(new PointView(), this.pointsListComponent.getElement());
+    const pointEditProperties = {
+      point: this.points[0],
+      destinations: this.destinations,
+      offers: this.offers
+    };
+    render(new PointEditView(pointEditProperties), this.pointsListComponent.getElement());
+
+    for (let i = 1; i < this.points.length; i++) {
+      const properties = {
+        point: this.points[i],
+        destinations: this.destinations,
+        offers: this.offers
+      };
+      render(new PointView(properties), this.pointsListComponent.getElement());
     }
 
   }
