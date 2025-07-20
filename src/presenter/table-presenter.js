d4ab2e035860471530b094ff47c4d01e646a7c7d
chlvk <ochlvk@gmail.com>
2025-07-20 15:00:17 +0300

    Добавляет презентеру приватные поля

--- src/presenter/table-presenter.js
+++ src/presenter/table-presenter.js
@@ -5,40 +5,47 @@ import PointsListView from '../view/points-list-view.js';
 import PointView from '../view/point-view.js';
 
 class TablePresenter {
+  #tableContainer = null;
+
   #pointsModel = null;
   #destinationsModel = null;
   #offersModel = null;
-  pointsListComponent = new PointsListView();
+
+  #pointsListComponent = new PointsListView();
+
+  #points = [];
+  #destinations = [];
+  #offers = [];
 
   constructor({container, pointsModel, destinationsModel, offersModel}) {
-    this.container = container;
+    this.#tableContainer = container;
     this.#pointsModel = pointsModel;
     this.#destinationsModel = destinationsModel;
     this.#offersModel = offersModel;
   }
 
   init() {
-    this.points = [...this.#pointsModel.points];
-    this.destinations = [...this.#destinationsModel.destinations];
-    this.offers = [...this.#offersModel.offers];
+    this.#points = [...this.#pointsModel.points];
+    this.#destinations = [...this.#destinationsModel.destinations];
+    this.#offers = [...this.#offersModel.offers];
 
-    render(new ListSortView(), this.container);
-    render(this.pointsListComponent, this.container);
+    render(new ListSortView(), this.#tableContainer);
+    render(this.#pointsListComponent, this.#tableContainer);
 
     const pointEditProperties = {
-      point: this.points[0],
-      destinations: this.destinations,
-      offers: this.offers
+      point: this.#points[0],
+      destinations: this.#destinations,
+      offers: this.#offers
     };
-    render(new PointEditView(pointEditProperties), this.pointsListComponent.element);
+    render(new PointEditView(pointEditProperties), this.#pointsListComponent.element);
 
-    for (let i = 1; i < this.points.length; i++) {
+    for (let i = 1; i < this.#points.length; i++) {
       const properties = {
-        point: this.points[i],
-        destinations: this.destinations,
-        offers: this.offers
+        point: this.#points[i],
+        destinations: this.#destinations,
+        offers: this.#offers
       };
-      render(new PointView(properties), this.pointsListComponent.element);
+      render(new PointView(properties), this.#pointsListComponent.element);
     }
 
   }
